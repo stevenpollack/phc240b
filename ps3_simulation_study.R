b708e5e24661c385e34f6cc55c5dd0eedaefdaf9
Steven Pollack <steven@pollackphoto.net>
2013-10-27 14:00:20 -0700

    modified IC.Pn to internally handle vectorization over both arguments.

--- ps3_simulation_study.R
+++ ps3_simulation_study.R
@@ -1,7 +1,7 @@
 library(parallel)
 num.of.cores <- detectCores()
 
-library(survival)
+#library(survival) # don't really need this
 
 #### simulation study
 # Lambda_n = Nelson-Aalen estimator
@@ -13,7 +13,7 @@ n <- c(200,500,1000) # number of U_i's in \U_n, and O_i affects accuracy of lim 
 
 ### generate data
 set.seed(5)
-latent.data <- rbind(rexp(n=n[1],rate=1),rexp(n=n[1],rate=0.5))
+latent.data <- rbind(rexp(n=n[2],rate=1),rexp(n=n[2],rate=0.5))
 O <- apply(X=latent.data,MARGIN=2,FUN=function(col){
   t <- col[1]; c <- col[2]
   if (t <= c) {
@@ -25,49 +25,48 @@ O <- apply(X=latent.data,MARGIN=2,FUN=function(col){
 #O <- t(O) # O = (Y,Delta)
 O <- O[,order(O[1,])] # make our lives easier by sorting
 
-U <- (replicate(n=m,expr=rnorm(n=n[1],mean=0,sd=1))) # U[,i] = raw data for \U_{i,n}
+U <- (replicate(n=m,expr=rnorm(n=n[2],mean=0,sd=1))) # U[,i] = raw data for \U_{i,n}
 
 ### process data
 Y <- O[1,]; Delta <- O[2,]
 n.risk <- sapply(X=Y,FUN=function(y.i){sum(Y >= y.i)})
-Pn <- ecdf(Y)
 S.hat <- Vectorize(FUN=function(t){mean(Y >= t)},vectorize.args=c("t"))
+S.n <- stepfun(x=Y,y=c(1,S.hat(Y)))
 
-### build grid
+### build parsimonious grid
 # J_0 = (1,2)
-grid.res <- 100
-J_0.grid.uniform <- seq(from=1,to=2,length.out=grid.res)
-J_0.grid.failure <- Y[(1 <= Y & Y <= 2 & Delta == 1)]
-J_0.grid <- sort(unique(c(J_0.grid.uniform,J_0.grid.failure)))
+# based on where we actually see failures
 
+# grid.res <- 100 # deprecated
+# J_0.grid.uniform <- seq(from=1,to=2,length.out=grid.res) # deprecated
+
+J_0.grid.failure <- Y[(1 <= Y & Y <= 2 & Delta == 1)]
+J_0.grid <- unique(c(1,J_0.grid.failure,2))
+# check grid: plot(J_0.grid,rep(x=1,times=length(J_0.grid))
 
 ### estimate influence curve
 ### IC.Pn(t;o) = a(t;o) - b(t;o)
 
-b <- cumsum( Delta / (S.hat(Y)*n.risk) )
-IC.Pn <- function(o,t) {
-  y <- o[1]; delta <- o[2]
-  sapply(X=t,FUN=function(t){
-    a <- delta * as.numeric( 0 <= y & y <= t) / S.hat(y)
-    m <- which.min(Y <= min(t,y)) - 1
-    return(a-b[m])
-  })
+b <- cumsum( Delta / (S.n(Y)*n.risk) )
+IC.Pn <- function(O,t) {
+  ### helper function
+  IC.1 <- function(t,y,delta) {
+    sapply(X=t,FUN=function(t){
+      a <- delta * as.numeric( 0 <= y & y <= t) / S.n(y)
+      m <- which.min(Y <= min(t,y)) - 1
+      return(a-b[m])
+    })
+  }
+           
+  ### logic to handle overloading
+  if (!is.null(dim(O))) { # passing matrix of O's
+    apply(X=O,MARGIN=2,FUN=function(o,times){IC.1(times,y=o[1],delta=o[2])},times=t)
+  } else { # passing just a single observation
+    IC.1(t,y=O[1],delta=O[2])
+  }
 }
 
-# ---------------------------
-# IC.Pn2 <- function(o) {
-#   y <- o[1]; delta <- o[2]
-#   function(t) {
-#     a <- delta * as.numeric(0 <= y & y <= t) / S.hat(y)
-#     b <- sum(sapply(X=1:n[1],FUN=function(i){
-#       Delta[i] * as.numeric(Y[i] <= min(t,y)) / (n[1] * S.hat(Y[i])^2 )
-#     }),na.rm=T)
-#     a-b
-#   }
-# }
-# -------------------------
-
-system.time(IC.mat <- apply(X=O,MARGIN=2,FUN=function(o){IC.Pn(o,J_0.grid)})) # 3.7 sec.
+system.time(IC.mat <- IC.Pn(O,J_0.grid)) # 1.316 sec.
 
 ### estimate variance of influence curve
 sigma.n <- Vectorize(FUN=function(t) {
@@ -75,6 +74,10 @@ sigma.n <- Vectorize(FUN=function(t) {
   b[m]
 })
 
+### check that this is right:
+emp <- apply(X=IC.mat,MARGIN=1,FUN=function(row){mean(row^2)})
+theor <- sigma.n(J_0.grid)
+max(abs(emp-theor))
 system.time(sigma.n(J_0.grid)) # 0.002sec
 
 ### estimate \U_{k,n}(t)
@@ -88,7 +91,7 @@ mathbb.U <- function(k,n) {
 
 ### build vector of \sup|\U_k's|
 system.time(expr={sup.U.n <- mclapply(X=1:100,mc.cores=num.of.cores,FUN=function(k){
-  max(abs(sapply(X=J_0.grid,FUN=function(t){mathbb.U(k,n[1])(t)})))
+  max(abs(sapply(X=J_0.grid,FUN=function(t){mathbb.U(k,n[2])(t)})))
 })})
 
 
